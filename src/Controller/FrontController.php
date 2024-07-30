65926250c7f978797034c026844bc2d4e81d4790
kiritoM17 <mvemvearnoldjunior@gmail.com>
2024-07-30 08:10:28 +0100

    newsletter

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Newsletter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,14 +13,21 @@ use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
 use Symfony\Component\Mime\Email;
 use Symfony\Component\Mime\Address;
+/* database */
+use Doctrine\Persistence\ManagerRegistry;
 
 class FrontController extends AbstractController
 {
     public function __construct(
+        private readonly ManagerRegistry $doctrine,
         private readonly MailerInterface $mailer
     ) {
     }
 
+    public function getDoctrine(): mixed
+    {
+        return $this->doctrine->getManager();
+    }
 
     #[Route('/', name: 'index_home_page', methods: ['GET'])]
     public function index(): Response
@@ -61,5 +69,51 @@ class FrontController extends AbstractController
         return $this->render('front/contact.html.twig', [
             'controller_name' => 'FrontController',
         ]);
+    }
+
+    #[Route('/newsletter', name: 'index_newsletter_page', methods: ['POST'])]
+    public function newsletter(Request $request): Response
+    {
+        if ($request->getMethod() == 'POST') {
+            $em = $this->getDoctrine();
+
+            $name = $request->get('name');
+            $email = $request->get('email');
+            if ($name && $email ) {
+                $newsletter = $em->getRepository(Newsletter::class)->findOneBy([
+                    'email' => $email
+                ]);
+                if(!$newsletter){
+                    $newsletter = new Newsletter();
+                    $newsletter->setName($name);
+                    $newsletter->setEmail($email);
+                    $em->persist($newsletter);
+                    $em->flush();
+
+                    /*
+                     * Envoie email ajout à la newsletter
+                     */
+                    $email = (new Email())
+                        ->from('contact@marketin.com')
+                        //->replyTo(new Address($email, $name))
+                        ->to($email)
+                        ->subject('Ajout à la newsletter')
+                        ->html("<p>Chers clients,</p><p>Chez MarketIn, nous sommes ravis de vous offrir des contenus exclusifs, des conseils marketing de pointe, et des offres spéciales directement dans votre boîte mail.</p><p>Nous sommes impatients de vous compter parmi nos abonnés.<br/>Cordialement,<br/>L'équipe MarketIn</p>");
+                    if ($request->files->get('inputAttachment')) {
+                        $email->attach(file_get_contents($request->files->get('inputAttachment')->getPathname()), $request->files->get('inputAttachment')->getClientOriginalName());
+                    }
+                    try {
+                        $this->mailer->send($email);
+                        $this->addFlash('success', 'vous avez été ajouté à la newsletter');
+                    } catch (TransportExceptionInterface $e) {
+                        $this->addFlash('error', 'Une erreur est survenue lors de l\'envoie de l\'email, merci de rééssayer');
+                    }
+                }
+            } else {
+                $this->addFlash('error', "Tous les champs doivent être renseignés");
+            }
+        }
+
+         return $this->redirect($request->headers->get('referer'));;
     }
 }
